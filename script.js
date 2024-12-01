edc0e758ca06b6d2bed4780ea24a00b1cd545e9d
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-01 09:59:02 +0100

    add : FizzBuzz

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 /*Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
+
+
+for (let i=1; i <= 100; i++) {
+    // controllo se (i) sia divisibile sia per tre che per 5
+ if (!(i % 3) && !(i % 5)) {
+    // se la condizione viene verificata viene stampato (FizzBuzz)
+    console.log("FizzBuzz");
+ }  else {
+    console.log(i);
+}
+}
